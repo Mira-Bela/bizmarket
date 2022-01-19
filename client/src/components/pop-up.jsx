e0f32a2e49bacb616426b08e5bec3cf267eb00a1
Mirabela Butiseaca <43749486+Mira-Bela@users.noreply.github.com>
2022-01-19 06:30:15 +0000

    finished styling for popup

--- client/src/components/pop-up.jsx
+++ client/src/components/pop-up.jsx
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { Context } from "./Context/context";
+import "./pop-up.css";
 
 const PopUp = (props) => {
+  const toggle = useContext(Context);
   const [text, setText] = useState({
     name: "",
     password: "",
   });
 
   return (
-    <div>
-      <div>
-        <span>x</span>
+    <div className="popup-box">
+      <div className="box">
+        <span className="close-icon" onClick={toggle.togglePopUp}>
+          x
+        </span>
         <b>BizMarket</b>
+        
       </div>
     </div>
   );
